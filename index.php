97219576a958051c46c3ceda7936a40b5c6f8096
Ayush Nagar <32647423+ayushnagar123@users.noreply.github.com>
2019-04-11 21:56:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
   <head><title>Jaypee Literary Seminar</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script>
-$(document).ready(function(){
+/*$(document).ready(function(){
    
    $('.brosher').load("brosher/index.html");
 
-});
+});*/
 </script>
   </head>
   <body>
     <h1>Jaypee Literary seminar 2019</h1>
-    <div class="brosher"></div>
+    <div class="brosher"><link rel="import" href="brosher/index.html"></div>
   <body>
 </html>
