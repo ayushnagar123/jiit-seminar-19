e9ac8a9845426ae5cc6f38e051a4b47ab8755716
Ayush Nagar <32647423+ayushnagar123@users.noreply.github.com>
2019-04-13 03:18:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,23 @@
   </head>
   <body>
     <h1>Jaypee Literary seminar 2019</h1>
+	<div id="flipbook">
+		<div class="hard"> Turn.js </div>
+		<div class="hard"></div>
+		<div> Page 1 </div>
+		<div> Page 2 </div>
+		<div> Page 3 </div>
+		<div> Page 4 </div>
+		<div class="hard"></div>
+		<div class="hard"></div>
+	</div>
+
+	<script type="text/javascript">
+		$("#flipbook").turn({
+			width: 400,
+			height: 300,
+			autoCenter: true
+		});
+	</script>
   <body>
 </html>
