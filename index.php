058a47e5773be1b0a1dbc762e8d9483c8ebf0480
Ayush Nagar <32647423+ayushnagar123@users.noreply.github.com>
2019-04-14 00:53:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -972,7 +972,9 @@
           </div>
         </div>
         <div class="row mb-30 text-center wow fadeInDown" data-wow-delay="0.3s">
-          <div class="col-md-3 col-sm-3 col-xs-12">
+          <div class="sponsors"><p>Comming Soon!</p>
+          <!--
+           <div class="col-md-3 col-sm-3 col-xs-12">
             <div class="spnsors-logo">
               <a href="#"><img class="img-fluid" src="assets/img/sponsors/logo-01.png" alt=""></a>
             </div>
@@ -992,6 +994,7 @@
               <a href="#"><img class="img-fluid" src="assets/img/sponsors/logo-04.png" alt=""></a>
             </div>
           </div>
+          -->
           <div class="col-12 text-center">
             <a href="#" class="btn btn-common">become a sponsor</a>
           </div>
