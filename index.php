c168356610d9fec0887cc130b9d093c2a7d6fa65
Ayush Nagar <32647423+ayushnagar123@users.noreply.github.com>
2019-04-14 16:23:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,20 @@
     <!-- Main Style -->
     <link rel="stylesheet" type="text/css" href="assets/css/main.css">
     <!-- Responsive Style -->
-    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+    <link rel="style sheet" type="text/css" href="assets/css/responsive.css">
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
 
   </head>
   <body>
@@ -518,21 +531,53 @@
             <div id="conatiner-map">
                                     <div id="map"></div>
     <script>
+      // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA2-eRizZfgwTulNj0e6nJpaWIMFS2ZK1g&libraries=places">
+
+      var map;
+      var service;
+      var infowindow;
+
       function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
+        var sydney = new google.maps.LatLng(-33.867, 151.195);
+
+        infowindow = new google.maps.InfoWindow();
+
+        map = new google.maps.Map(
+            document.getElementById('map'), {center: sydney, zoom: 15});
+
+        var request = {
+          query: 'Museum of Contemporary Art Australia',
+          fields: ['name', 'geometry'],
+        };
+
+        service = new google.maps.places.PlacesService(map);
+
+        service.findPlaceFromQuery(request, function(results, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              createMarker(results[i]);
+            }
+
+            map.setCenter(results[0].geometry.location);
+          }
         });
+      }
+
+      function createMarker(place) {
         var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
         });
       }
     </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA2-eRizZfgwTulNj0e6nJpaWIMFS2ZK1g&callback=initMap">
-                                           </script>
+
                                                                                                               </div>
           </div>
         </div>
