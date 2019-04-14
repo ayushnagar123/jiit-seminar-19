961885931bb790e5f692f606af1ed7f3a863bcff
Ayush Nagar <32647423+ayushnagar123@users.noreply.github.com>
2019-04-14 16:38:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -643,7 +643,7 @@
         <div class="row">
           <div class="col-md-6 col-lg-3 col-sm-6 col-xs-12 wow fadeInUp" data-wow-delay="0.2s">
             <h3><img src="assets/img/logo2.png" alt=""></h3>
-            <p>
+            <p style="text-align:center;">
               The Seminar's primary objective is to instigate a research based thinking in the young minds and help them achieve success.
             </p>
           </div>
@@ -662,7 +662,6 @@
               <h5 class="widget-title">FOLLOW US ON</h5>
               <ul class="footer-social">
                 <li><a class="facebook" href="#"><i class="lni-facebook-filled"></i></a></li>
-                <li><a class="google-plus" href="#"><i class="lni-google-plus"></i></a></li>
               </ul>
             </div>
           </div>
